c9d3c93004b75d234f207520c8c99e32df4c2081
Gott <stbn.schn@gmail.com>
2021-05-02 19:01:33 -0300

    Practice JS

---
+++ practice.js
@@ -0,0 +1,17 @@
+function add7(){
+    let number = 2;
+
+    return number + 7;
+}
+
+console.log(add7);
+
+
+function multiply (number,number2){
+    number = 20;
+    number2 = 2;
+
+    return number * number2
+}
+
+console.log(multiply());
